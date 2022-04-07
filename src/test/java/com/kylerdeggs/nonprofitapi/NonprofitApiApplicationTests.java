2b78669a1f903da338a14c511a53203ce482cf83
Kyler Deggs <deggskyler@icloud.com>
2022-04-06 19:30:26 -0500

    Initial commit

---
+++ src/test/java/com/kylerdeggs/nonprofitapi/NonprofitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kylerdeggs.nonprofitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonprofitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
